e4f8b64538d351f81457f133feadacbe3a1db4f3
maximum-lie3625 <wickedevil3625@gmail.com>
2023-11-28 16:18:32 +0530

    fixup! new-changes 25-nov

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const  http = require('http');
+const http = require('http');
+const fs = require('fs');
 
-const  server = http.createServer((req, res)=>{
-    if (req.url=== '/') {
-        res.end('Welcome to our home page')
-    }
-    else if(req.url==='/about'){
-        res.end('Here is our short history')
-    }
-    else res.end('<h1>\'Oops!\'</h1>\n' +
-        '    <p>\'We can\'t seem to find the page your loocking for\'</p>\n' +
-        '    <a href="/">back home</a>')
+http.createServer(function (req, res){
+    const fileStream = fs.createReadStream('./CONTENT/big.txt', 'utf8');
+    fileStream.on('open',()=>{
+fileStream.pipe()
+    })
+    fileStream.on('error',(err)=>{
+        res.end(err)
+    })
 })
-server.listen(5000)
